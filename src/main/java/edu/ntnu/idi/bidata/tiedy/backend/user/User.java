6c33bc4362af61a35a5e846aed10def204c119b8
odinarvhage <odin.arvhage@outlook.com>
2025-02-27 14:56:30 +0100

    feat: added methods to the User class

--- src/main/java/edu/ntnu/idi/bidata/tiedy/backend/user/User.java
+++ src/main/java/edu/ntnu/idi/bidata/tiedy/backend/user/User.java
@@ -5,4 +5,65 @@ package edu.ntnu.idi.bidata.tiedy.backend.user;
  * @version 2025.02.26
  */
 public class User {
+  private int userID;
+  private String userName;
+  private String emailAddress;
+
+
+  public User(String userName, String emailAddress, int userID) {
+    setUserName(userName);
+    setEmailAddress(emailAddress);
+    setUserID(userID);
+  }
+
+  public void login(String userName, String emailAddress, int userID) {
+    if(checkUserInformation(userName, emailAddress, userID)){
+      //give access to the application
+    }
+  }
+
+  public boolean checkUserInformation(String userName, String emailAddress, int userID) {
+    if (checkUserName(userName) && checkEmailAddress(emailAddress)) {
+      return checkUserID(userID);
+    }
+    return false;
+  }
+
+  public boolean checkEmailAddress(String emailAddress) {
+    return emailAddress.equals(getEmailAddress());
+  }
+
+  public boolean checkUserID(int userID) {
+    return(userID == getUserID());
+  }
+
+  public boolean checkUserName(String userName) {
+    return(userName.equals(getUserName()));
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  public int getUserID() {
+    return userID;
+  }
+
+  public void setUserID(int userID) {
+    this.userID = userID;
+  }
 }
+
+

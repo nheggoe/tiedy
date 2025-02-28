92cda66134bf9642d615c1fb7126342bafb41c1c
Nick Heggø <nickhe@stud.ntnu.no>
2025-02-28 11:55:55 +0100

    refactor: clean up code style and enhance test reliability

--- src/test/java/edu/ntnu/idi/bidata/tiedy/backend/util/JSONWriterTest.java
+++ src/test/java/edu/ntnu/idi/bidata/tiedy/backend/util/JSONWriterTest.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
 class JSONWriterTest {
 
@@ -24,6 +25,19 @@ class JSONWriterTest {
   @Test
   void testWriteToResource() {
     assertDoesNotThrow(() -> objectMapper.writeValue(new File(userJsonFile), users));
+  }
+
+  void test() {
+    List<String> style1 = List.of(
+        "one",
+        "two",
+        "tree");
+
+    List<String> style2 = List.of(
+        "one",
+        "two",
+        "tree"
+    );
   }
 
 }

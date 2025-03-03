0e6dbb516f998b1abaa87d95c6eb2710dd488ebf
Ida <idaclo@stud.ntnu.no>
2025-03-03 13:38:08 +0100

    test(TaskTest) Tests for id

--- src/test/java/edu/ntnu/idi/bidata/tiedy/backend/level/TaskTest.java
+++ src/test/java/edu/ntnu/idi/bidata/tiedy/backend/level/TaskTest.java
@@ -5,8 +5,11 @@ import edu.ntnu.idi.bidata.tiedy.backend.task.Status;
 import edu.ntnu.idi.bidata.tiedy.backend.task.Task;
 import edu.ntnu.idi.bidata.tiedy.backend.user.User;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
     private Task task;
@@ -17,5 +20,22 @@ class TaskTest {
         user = new User();
         task = new Task(1,"Test Task","Task used for testing"
                 , Status.IN_PROGRESS, user, LocalDate.now().plusDays(7), Priority.HIGH);
+    }
+
+    @Test
+    void testGetId() {
+        assertEquals(1, task.getId());
+    }
+
+    @Test
+    void testSetId() {
+        task.setId(2);
+        assertEquals(2, task.getId());
+    }
+
+    @Test
+    void testSetInvalidId() {
+        assertThrows(IllegalArgumentException.class, () -> task.setId(0));
+        assertThrows(IllegalArgumentException.class, () -> task.setId(-1));
     }
 }

ad0654932fd29c90f6bd760f960c7b59bac7a08a
Nick Heggø <nickhe@stud.ntnu.no>
2025-03-09 22:47:08 +0100

    feat!: replace H2 database with JSON-based user storage

--- src/main/java/edu/ntnu/idi/bidata/tiedy/Launcher.java
+++ src/main/java/edu/ntnu/idi/bidata/tiedy/Launcher.java
@@ -1,6 +1,5 @@
 package edu.ntnu.idi.bidata.tiedy;
 
-import edu.ntnu.idi.bidata.tiedy.backend.util.DatabaseUtil;
 import edu.ntnu.idi.bidata.tiedy.frontend.stage.JavaFXApp;
 
 /**
@@ -32,7 +31,6 @@ public class Launcher {
    * @param args the command-line arguments passed to the application
    */
   public static void main(String[] args) {
-    DatabaseUtil.initDatabase();
     JavaFXApp.main(args);
   }
 }

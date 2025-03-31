16fdc8ed6fd8a8ee83702cf9370124969b70b369
Odin Arvhage <odin.arvhage@outlook.com>
2025-03-31 12:55:28 +0200

    feat: added the ExitController and the Exit fxml file

---
+++ src/main/java/edu/ntnu/idi/bidata/tiedy/frontend/controller/ExitController.java
@@ -0,0 +1,26 @@
+package edu.ntnu.idi.bidata.tiedy.frontend.controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * The ExitController class is used to manage the pop-up exit window that appears when the exit button is pressed.
+ * @author odinarvhage
+ * @version 31-03-2025
+ */
+public class ExitController {
+
+    /**
+     * This method is called when the "Yes" button is pressed. The application will then terminate.
+     */
+    @FXML
+    public void onYesPressed() {
+    System.exit(0);
+}
+    /**
+     * This method is called when the "Yes" button is pressed. The pop-up window will then close.
+     */
+@FXML
+    public void onNoPressed() {
+    //close the pop-up window
+}
+}
